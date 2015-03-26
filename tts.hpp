8d7477f780c5a750b2fe4926267a3510f500ec72
Dakota <drw9888@rit.edu>
2015-03-26 02:50:41 +0000

    fix for alsa, actually works instead of theoretically working

--- tts.hpp
+++ tts.hpp
@@ -8,6 +8,10 @@ private:
 public:
 	static void init() {
 		festival_initialize(1, TTS::heap_size);
+		//for alsa
+		festival_eval_command("(Parameter.set 'Audio_Method 'Audio_Command)");
+		festival_eval_command("(Parameter.set 'Audio_Command \"aplay -q -c 1 -t raw -f s16 -r $SR $FILE\")");
+		//end for alsa
 	}
 
 	static void say(const char* text) {
