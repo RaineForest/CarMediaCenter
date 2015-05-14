49447c193a3a131e48297dbd7e529d4d62be9f5d
Dakota Williams <dwms9395@verizon.net>
2015-05-13 22:22:09 -0400

    support for pulse audio

--- tts.hpp
+++ tts.hpp
@@ -1,6 +1,8 @@
 
 #include "festival.h"
 
+#define USE_PULSE
+
 class TTS {
 private:
 	const static int heap_size = 2100000;
@@ -8,10 +10,12 @@ private:
 public:
 	static void init() {
 		festival_initialize(1, TTS::heap_size);
-		//for alsa
 		festival_eval_command("(Parameter.set 'Audio_Method 'Audio_Command)");
+#ifdef USE_PULSE
+		festival_eval_command("(Parameter.set 'Audio_Command \"paplay $FILE --client-name=Festival --stream-name=Speech\")");
+#elif USE_ALSA
 		festival_eval_command("(Parameter.set 'Audio_Command \"aplay -q -c 1 -t raw -f s16 -r $SR $FILE\")");
-		//end for alsa
+#endif
 	}
 
 	static void say(const char* text) {
